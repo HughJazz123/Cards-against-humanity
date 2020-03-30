c56d942b78b9836bca4b8f3ced353e6d0586f2da
HughJazz123 <50455574+HughJazz123@users.noreply.github.com>
2020-03-30 13:51:39 +0800

    Update cards_against_humanity.py

--- cards_against_humanity.py
+++ cards_against_humanity.py
@@ -1,108 +1,87 @@
 import random
 
-
 black_cards = open("black_cards.txt").read().split("\n")
-
 white_cards = open("white_cards.txt").read().split("\n")
-
 
 def space():
     print("\n"*40)
 space()
 print("White cards:",len(white_cards))
 print("Black cards:",len(black_cards))
-p=int(input("Number of players (4-7): "))
+p=int(input("Number of players (4+): "))
 
-p_=['p1','p2','p3','p4','p5','p6','p7']
- 
+while p<4:
+    print("minimum 4 players only.\n")
+    p=int(input("Number of players (4+): "))
+print()
 score_keeper=[]
 players=[]
-namelist=[]
-
-space()
+player_n_cards=[]
+player_cards=[]
 for i in range(p):
-    players.append(p_[i])
-    score_keeper.append(p_[i])
+    name=input("Player "+str(i+1)+": ")
+    players.append(name)
+    score_keeper.append(0)
+    for i in range(10):
+        random_card=random.choice(white_cards)
+        player_n_cards.append(random_card)
+        white_cards.remove(random_card)
+    player_cards.append(player_n_cards)
+    player_n_cards=[]
 
-    score_keeper[i]=0
-
-    print("Enter name (Player",i+1,"):")
-    name=input()
-    
-    namelist.append(name)
-    print()
-
-    players[i]=[]
-    a=i
-    for c in range(10):
-        card=random.choice(white_cards)
-        players[a].append(card)
-        white_cards.remove(card)
-a=0#card czar
-s=""
-space()
-
-
-
-while s!="q":
-    g=0#to take turns between players
-    
-    white_cards_players=[]#i have no idea what this is
-    players_white_cards=[]#i have no idea what this is either
-    non_card_czars=[]#tf is the difference
+card_czar_number=0
+while True:
+    selected_cards=[]
+    non_card_czars=players[:]
+    non_card_czars.pop(card_czar_number)
     
     black_card=random.choice(black_cards)
     black_cards.remove(black_card)
-    while g<p:
-        if a!=g:
-            print(namelist[g],"'s turn:")
-            non_card_czars.append(namelist[g])
-            input()
-            print("Black card:",black_card)
-            print()
-            for i in range(10):
-                print(str(i+1)+". "+players[g][i])
-
-            choice=int(input())
-            choice=choice-1
-            players_white_cards.append(players[g][choice])
-            white_cards_players.append(players[g][choice])
-            players[g].pop(choice)
-        g+=1
-        space()
-    
-    print("Black card:",black_card)
-    print("Card Czar:",namelist[a])
-    input()
-    white_cards_in_order=[]
-    c1=1
-    for i in range(p-1):
-        c=random.randint(0,len(players_white_cards)-1)
-        print("Card "+str(c1)+": "+str(players_white_cards[c]))
-        
-        white_cards_in_order.append(players_white_cards[c])
-        players_white_cards.pop(c)
-        c1+=1
-
-    card_czar_choice=int(input("Card (Enter the number of your choice): "))
-    winner_card = white_cards_in_order[card_czar_choice-1]
-    winner=white_cards_players.index(winner_card)
-    
-    print(non_card_czars[winner],"wins!")
-
-    w=namelist.index(non_card_czars[winner])
-    score_keeper[w]+=1
-    
-    s=input("Press Enter to continue (or 'q' to exit)\n")
-    space()
-    b=0
     for i in range(p):
-        if b!=g and b<p:
-            r=random.choice(white_cards)
-            players[i].append(r)
-            white_cards.remove(r)
-    a+=1
-    if a==p:
-        a=0
-for i in range(p):
-    print(str(namelist[i])+": "+str(score_keeper[i])+" point(s)")
+        if i!=card_czar_number:
+            space()
+            input((players[i])+"'s turn (Press enter to continue)\n")
+            print("Black card: "+black_card+"\n")
+            
+            for x in range(10):
+                print("Card "+str(x+1)+": "+player_cards[i][x])
+            #selection process
+            selection=int(input("\nSelect the number of the card you wish to pick: "))
+            selection-=1
+            selected_cards.append(player_cards[i][selection])
+            player_cards[i].pop(selection)
+
+            #adding another card
+            adding_white_card=random.choice(white_cards)
+            player_cards[i].append(adding_white_card)
+            white_cards.remove(adding_white_card)
+    space()
+    input("Card Czar: "+players[card_czar_number]+"\n")
+    shuffled_cards=selected_cards[:]
+    random.shuffle(shuffled_cards)
+    print("Black card: "+black_card+"\n")
+    for i in range(len(shuffled_cards)):
+        print("Card "+str(i+1)+": "+shuffled_cards[i])
+    ##card czar selecting
+    card_czar_selection=int(input("\nYour selection:\nCard: "))
+    card_czar_selection-=1
+    
+    #card czar selected
+    winner_card_number=selected_cards.index(shuffled_cards[card_czar_selection])
+    winner_of_round=non_card_czars[winner_card_number]
+    
+    score_keeper[players.index(winner_of_round)]+=1
+
+    if card_czar_number!=(len(players)-1):
+        card_czar_number+=1
+    else:
+        card_czar_number=0
+    print(winner_of_round+" wins!\n")
+    quit_game=input("Enter 'q' to if you wish to quit, or else, press Enter: ")
+    
+    if quit_game=="q":
+        space()
+        print("Final scores:")
+        for i in range(p):
+            print(players[i]+": "+str(score_keeper[i])+" points")
+        break
